718b85ef5e321679b9ec9417feaab96d9ac044b1
clariserni <clarissaserni.96@gmail.com>
2024-06-26 15:34:22 +0200

    update

--- src/App.java
+++ src/App.java
@@ -1,4 +1,49 @@
 public class App {
+    /**
+ * TODO:
+ * 
+ * ES0: Creare un repository su GitHub con il seguente nome:
+ * 2024-06-26-java-end-1
+ * 
+ * Caricare tutti gli esercizi in un unico progetto sulla stessa repo
+ * 
+ * 
+ * ES1: Sistema di Prenotazioni
+ * 
+ * Crea una classe Prenotazione che possa essere utilizzata da un teatro per
+ * tenere traccia delle prenotazioni dei posti. Utilizza un array di booleani
+ * per rappresentare i posti: true se il posto è prenotato, false altrimenti.
+ * Implementa metodi per prenotare un posto, annullare la prenotazione e
+ * verificare se un o piu' posti sono disponibili.
+ * 
+ * ---------------------------------------------------------------------------
+ * 
+ * ES2: Registro Studenti
+ * 
+ * Crea una classe Studente con attributi come nome, matricola, e voti (usa un
+ * ArrayList). Aggiungi metodi per aggiungere voti, calcolare la media dei voti,
+ * e stampare i dettagli dello studente. Implementa la gestione delle eccezioni
+ * per evitare errori nell'inserimento dei voti.
+ * 
+ * ---------------------------------------------------------------------------
+ * 
+ * ES3: Simulatore di Banca
+ * 
+ * Definisci una classe ContoBancario con metodi per depositare denaro,
+ * prelevare denaro e ottenere il saldo. Assicurati di gestire il caso in cui si
+ * tenta di prelevare più denaro di quanto disponibile con un'eccezione.
+ * 
+ * ---------------------------------------------------------------------------
+ * 
+ * ES4: Elenco Telefonico
+ * 
+ * Implementa una classe Contatto che rappresenti un contatto telefonico con
+ * nome e numero di telefono (stringa). Crea una classe ElencoTelefonico che gestisca una
+ * lista di contatti. Includi metodi per aggiungere, rimuovere e cercare
+ * contatti. Gestisci eventuali eccezioni relative all'inserimento di numeri di
+ * telefono non validi.
+ * 
+ */
     public static void main(String[] args) throws Exception {
 
         Studente();
@@ -6,6 +51,8 @@ public class App {
         contoBancario();
         System.out.println("---------------------------");
         elencoTelefonico();
+        System.out.println("---------------------------");
+        prenotazioni();
     }
 
     public static void Studente() {
@@ -65,49 +112,16 @@ public class App {
         Contatto cerco = elenco.cercaContatti("Clarissa");
         System.out.println(cerco);
     }
-/**
- * TODO:
- * 
- * ES0: Creare un repository su GitHub con il seguente nome:
- * 2024-06-26-java-end-1
- * 
- * Caricare tutti gli esercizi in un unico progetto sulla stessa repo
- * 
- * 
- * ES1: Sistema di Prenotazioni
- * 
- * Crea una classe Prenotazione che possa essere utilizzata da un teatro per
- * tenere traccia delle prenotazioni dei posti. Utilizza un array di booleani
- * per rappresentare i posti: true se il posto è prenotato, false altrimenti.
- * Implementa metodi per prenotare un posto, annullare la prenotazione e
- * verificare se un o piu' posti sono disponibili.
- * 
- * ---------------------------------------------------------------------------
- * 
- * ES2: Registro Studenti
- * 
- * Crea una classe Studente con attributi come nome, matricola, e voti (usa un
- * ArrayList). Aggiungi metodi per aggiungere voti, calcolare la media dei voti,
- * e stampare i dettagli dello studente. Implementa la gestione delle eccezioni
- * per evitare errori nell'inserimento dei voti.
- * 
- * ---------------------------------------------------------------------------
- * 
- * ES3: Simulatore di Banca
- * 
- * Definisci una classe ContoBancario con metodi per depositare denaro,
- * prelevare denaro e ottenere il saldo. Assicurati di gestire il caso in cui si
- * tenta di prelevare più denaro di quanto disponibile con un'eccezione.
- * 
- * ---------------------------------------------------------------------------
- * 
- * ES4: Elenco Telefonico
- * 
- * Implementa una classe Contatto che rappresenti un contatto telefonico con
- * nome e numero di telefono (stringa). Crea una classe ElencoTelefonico che gestisca una
- * lista di contatti. Includi metodi per aggiungere, rimuovere e cercare
- * contatti. Gestisci eventuali eccezioni relative all'inserimento di numeri di
- * telefono non validi.
- * 
- */
+
+    public static void prenotazioni(){
+         Prenotazione p = new Prenotazione();
+
+        System.out.println(p.prenota(1)); 
+        System.out.println(p.prenota(5));
+
+        System.out.println(p.annullaPrenotazione(1));
+        System.out.println("Disponibilità: "+p.isDisponibile(15));
+        System.out.println(p.toString());
+    }
+
 }
--- src/Prenotazione.java
+++ src/Prenotazione.java
@@ -2,9 +2,55 @@ import java.util.Arrays;
 
 public class Prenotazione {
 
-   
+   private boolean [] posti;
 
+    public Prenotazione() {
+        this.posti = new boolean[30];
+    }
 
+    public boolean prenota(int numeroPosto){
+        if(numeroPosto<0 || numeroPosto >= posti.length){
+            throw new IllegalArgumentException("Posto non valido");
+        }
+
+        if(!posti[numeroPosto]){
+            posti[numeroPosto] = true;
+            return true;
+        }
+        return false;
+
+    }
+
+    public boolean annullaPrenotazione(int numeroPosto){
+        if (numeroPosto < 0 || numeroPosto >= posti.length) {
+            throw new IllegalArgumentException("Posto non valido.");
+        }
+        if (posti[numeroPosto]) {
+            posti[numeroPosto] = false;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isDisponibile(int numeroPosto){
+        if (numeroPosto < 0 || numeroPosto >= posti.length) {
+            throw new IllegalArgumentException("Posto non valido.");
+        }
+
+        if (posti[numeroPosto]) 
+            return false;
+        else 
+            return true;
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Prenotazione [posti=" + Arrays.toString(posti) + "]";
+    }
+
+    
+    
 
     
 }
